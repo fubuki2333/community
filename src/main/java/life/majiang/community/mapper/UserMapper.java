bc3ae2837bc6de91bc4c8210db9811c26cb1ad1c
fubuki2333 <gaochenyu2012@126.com>
2019-12-01 23:56:33 +0800

    Create User class

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package life.majiang.community.mapper;
+
+/**
+ * ClassName:UserMapper
+ * Package:life.majiang.community.mapper
+ * Description:
+ *
+ * @Date:2019/12/1 下午 12:05
+ * @Author:gaochenyu2012@126.com
+ */
+public class UserMapper {
+}
---
+++ src/main/java/life/majiang/community/model/User.java
@@ -0,0 +1,12 @@
+package life.majiang.community.model;
+
+/**
+ * ClassName:User
+ * Package:life.majiang.community.model
+ * Description:
+ *
+ * @Date:2019/12/1 下午 2:35
+ * @Author:gaochenyu2012@126.com
+ */
+public class User {
+}
